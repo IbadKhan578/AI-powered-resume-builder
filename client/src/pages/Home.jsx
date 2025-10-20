e9cad9f2bbca0584078dbd98ea9b33d398314de1
Ibad Khan <ibadkhansherani@gmail.com>
2025-10-20 23:10:59 +0500

    Added testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/home/Banner'
 import HeroSection from '../components/home/HeroSection'
 import Feature from '../components/home/Feature'
+import Testimonals from '../components/home/Testimonals'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Banner />
     <HeroSection />
     <Feature />
+    <Testimonals />
     </>
   )
 }
