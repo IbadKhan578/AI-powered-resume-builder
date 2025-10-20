972ab38e377a91ff587db7ddbd99779946b8fe28
Ibad Khan <ibadkhansherani@gmail.com>
2025-10-20 22:38:57 +0500

    initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import HeroSection from '../components/home/HeroSection'
+import Feature from '../components/home/Feature'
+
+const Home = () => {
+  return (
+    <>
+    <Banner />
+    <HeroSection />
+    <Feature />
+    </>
+  )
+}
+
+export default Home
